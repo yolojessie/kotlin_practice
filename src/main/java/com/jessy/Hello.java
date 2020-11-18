9705b7b75377af89897f9a63c878d0047f9402b3
yolojessie <jessie840904@gmail.com>
2020-11-18 12:54:08 +0800

    Java & Kotlin variable, data type

--- src/main/java/com/jessy/Hello.java
+++ src/main/java/com/jessy/Hello.java
@@ -5,5 +5,16 @@ public class Hello {
 //        System.out.print("Hello World");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        double height = 1.7;
+        boolean afult = true;
+        boolean enroll = false;
+        String name = "Tom";
+
     }
 }
--- src/main/kotlin/com/jessy/Hello.kt
+++ src/main/kotlin/com/jessy/Hello.kt
@@ -5,6 +5,9 @@ fun main() {
 //    Human().hello()
     val h = Human()
     h.hello()
+    val age = 19; //不能改動
+    var age2 = 20; //自對推斷型態 可改動
+    var name : String; // 若沒要先給值就要預設型態
 }
 
 class Human{
